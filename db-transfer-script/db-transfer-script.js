8aa79c7ac77a8d9c4c7fa4421da493bd9dd89520
Dalton <meckl031@umn.edu>
2020-08-31 10:14:46 -0500

    db transfer script

---
+++ db-transfer-script/db-transfer-script.js
@@ -0,0 +1,88 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const knex_1 = __importDefault(require("knex"));
+const config_json_1 = __importDefault(require("../config.json"));
+const keyv = knex_1.default({
+    client: config_json_1.default.databaseType,
+    connection: {
+        host: config_json_1.default.databaseHost,
+        user: config_json_1.default.databaseUsername,
+        password: config_json_1.default.databasePassword,
+        database: 'mydatabase'
+    }
+});
+const knex = knex_1.default({
+    client: config_json_1.default.databaseType,
+    connection: {
+        host: config_json_1.default.databaseHost,
+        user: config_json_1.default.databaseUsername,
+        password: config_json_1.default.databasePassword,
+        database: 'queue'
+    }
+});
+knex.schema.hasTable('queue_guilds').then(exists => {
+    if (!exists)
+        knex.schema.createTable('queue_guilds', table => {
+            table.text('guild_id').primary();
+            table.text('grace_period');
+            table.text('prefix');
+            table.text('color');
+        }).catch(e => console.error(e));
+});
+knex.schema.hasTable('queue_channels').then(exists => {
+    if (!exists)
+        knex.schema.createTable('queue_channels', table => {
+            table.text('queue_channel_id').primary();
+            table.text('guild_id');
+        }).catch(e => console.error(e));
+});
+knex.schema.hasTable('queue_members').then(exists => {
+    if (!exists)
+        knex.schema.createTable('queue_members', table => {
+            table.increments('id').primary();
+            table.text('queue_channel_id');
+            table.text('queue_member_id');
+            table.text('personal_message');
+            table.timestamp('created_at').defaultTo(knex.fn.now());
+        }).catch(e => console.error(e));
+});
+knex.schema.hasTable('display_channels').then(exists => {
+    if (!exists)
+        knex.schema.createTable('display_channels', table => {
+            table.increments('id').primary();
+            table.text('queue_channel_id');
+            table.text('display_channel_id');
+            table.specificType('embed_ids', 'TEXT []');
+        }).catch(e => console.error(e));
+});
+keyv('keyv').then((keyvEntries) => __awaiter(void 0, void 0, void 0, function* () {
+    for (const keyvEntry of keyvEntries) {
+        const guildId = keyvEntry.key.replace('keyv:', '');
+        const pair = JSON.parse(keyvEntry.value);
+        yield knex('queue_guilds').insert({
+            guild_id: guildId,
+            grace_period: pair.value[0],
+            prefix: pair.value[1],
+            color: pair.value[2]
+        });
+        for (const queueChannelId of pair.value.slice(10)) {
+            yield knex('queue_channels').insert({
+                queue_channel_id: queueChannelId,
+                guild_id: guildId
+            });
+        }
+    }
+    console.log('Complete');
+}));
---
+++ db-transfer-script/db-transfer-script.ts
@@ -0,0 +1,105 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import Knex from 'knex';
+import config from "../config.json";
+
+// Connect DBs
+const keyv = Knex({
+	client: config.databaseType,
+	connection: {
+		host: config.databaseHost,
+		user: config.databaseUsername,
+		password: config.databasePassword,
+		database: 'mydatabase'
+		//database: config.databaseName
+	}
+});
+
+const knex = Knex({
+	client: config.databaseType,
+	connection: {
+		host: config.databaseHost,
+		user: config.databaseUsername,
+		password: config.databasePassword,
+		database: 'queue'
+	}
+});
+
+interface KeyvTable {
+	key: string;
+	value: string;
+}
+
+interface KeyvPair {
+	value: string[];
+	expires: string;
+}
+
+interface QueueGuild {
+	guild_id: string; // KEY
+	grace_period: string;
+	prefix: string;
+	color: string;
+}
+
+interface QueueChannel {
+	queue_channel_id: string; // KEY
+	guild_id: string;
+}
+
+// Setup new DB
+knex.schema.hasTable('queue_guilds').then(exists => {
+	if (!exists) knex.schema.createTable('queue_guilds', table => {
+		table.text('guild_id').primary();
+		table.text('grace_period');
+		table.text('prefix');
+		table.text('color');
+	}).catch(e => console.error(e));
+});
+knex.schema.hasTable('queue_channels').then(exists => {
+	if (!exists) knex.schema.createTable('queue_channels', table => {
+		table.text('queue_channel_id').primary();
+		table.text('guild_id');
+	}).catch(e => console.error(e));
+});
+knex.schema.hasTable('queue_members').then(exists => {
+	if (!exists) knex.schema.createTable('queue_members', table => {
+		table.increments('id').primary();
+		table.text('queue_channel_id');
+		table.text('queue_member_id');
+		table.text('personal_message');
+		table.timestamp('created_at').defaultTo(knex.fn.now());
+	}).catch(e => console.error(e));
+});
+knex.schema.hasTable('display_channels').then(exists => {
+	if (!exists) knex.schema.createTable('display_channels', table => {
+		table.increments('id').primary();
+		table.text('queue_channel_id');
+		table.text('display_channel_id');
+		table.specificType('embed_ids', 'TEXT []');
+	}).catch(e => console.error(e));
+});
+
+// Import Old DB
+keyv<KeyvTable>('keyv').then(async keyvEntries => {
+	for (const keyvEntry of keyvEntries) {
+
+
+		const guildId = keyvEntry.key.replace('keyv:', '');
+		const pair: KeyvPair = JSON.parse(keyvEntry.value);
+
+		await knex<QueueGuild>('queue_guilds').insert({
+			guild_id: guildId,
+			grace_period: pair.value[0],
+			prefix: pair.value[1],
+			color: pair.value[2]
+		});
+
+		for (const queueChannelId of pair.value.slice(10)) {
+			await knex<QueueChannel>('queue_channels').insert({
+				queue_channel_id: queueChannelId,
+				guild_id: guildId
+			});
+		}
+	}
+	console.log('Complete')
+})
