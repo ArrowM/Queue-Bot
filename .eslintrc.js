64150d8a80772e91a844bd911bba38bb2c002d26
Dalton <meckl031@umn.edu>
2020-11-22 16:04:29 -0600

    adding eslint file

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  plugins: [
+    'node',
+    'promise',
+    'import',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': ["warn", {
+        "endOfLine": "auto",
+        "printWidth": 140,
+        "tabWidth": 3
+    }],
+    'no-console': 'off'
+  }
+}
