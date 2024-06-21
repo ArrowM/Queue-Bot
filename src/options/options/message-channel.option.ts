9fa632c022f05e773d11bae2a444c585eaf8f259
ArrowM <arrowman.6677@gmail.com>
2024-06-20 21:45:42 -0500

    schedule and reset fixes

--- src/options/options/message-channel.option.ts
+++ src/options/options/message-channel.option.ts
@@ -2,7 +2,7 @@ import { TEXT_CHANNEL_TYPE } from "../../types/misc.types.ts";
 import { ChannelOption } from "../base-option.ts";
 
 export class MessageChannelOption extends ChannelOption {
-	static readonly ID = "message_channel_id";
+	static readonly ID = "message_channel";
 	id = MessageChannelOption.ID;
 	channelTypes = TEXT_CHANNEL_TYPE;
 }
